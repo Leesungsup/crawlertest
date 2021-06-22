77f945226362a4fb5a458567182c2396e224b091
Leesungsup <tmdtjq0116@naver.com>
2021-06-23 00:24:22 +0900

    staticsfiles

--- crawler/settings.py
+++ crawler/settings.py
@@ -123,7 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+AUTHENTICATION_BACKENDS=[
+'django.contrib.auth.backends.ModelBackend',
+'allauth.account.auth_backends.AuthenticationBackend',
+]
+SITE_ID=1
+#STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 MEDIA_URL='/uploads/'

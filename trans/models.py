32df32140a5bac6c1b31392b8ddbd151432d9644
Leesungsup <tmdtjq0116@naver.com>
2021-06-22 18:02:40 +0900

    settings

--- trans/models.py
+++ trans/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Player_info(models.Model):
+    number = models.IntegerField(primary_key = True)
+    name = models.CharField(max_length=20)
+    position = models.CharField(max_length=20)
+    age=models.IntegerField()
+    nation=models.CharField(max_length=20)
+    team=models.CharField(max_length=30)
+    value=models.CharField(max_length=30)

cfd1e7cba5508035de4b4a0687f600f40f4e0ca3
Leesungsup <tmdtjq0116@naver.com>
2021-06-23 00:45:30 +0900

    templates

--- crawler/views.py
+++ crawler/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
-from .models import Player_info
 import random
 # Create your views here.
 def hello(request):
     html="<html><body>Hello world!</body></html>"
     return HttpResponse(html)
 def test(request):
-    return render(request,'templates/test.html')
+    return render(request,'test.html')

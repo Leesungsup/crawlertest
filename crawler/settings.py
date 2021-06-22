8c88f4ae89ef0bc95cbb3d257cbfe7ed6f7e1f04
Leesungsup <tmdtjq0116@naver.com>
2021-06-22 23:50:48 +0900

    Media URL

--- crawler/settings.py
+++ crawler/settings.py
@@ -126,6 +126,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-MEDIA_URL='/uploads'
+MEDIA_URL='/uploads/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'uploads')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

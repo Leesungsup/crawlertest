b2ba54c663b5d7c4a8eda086b270e314e0f1f388
Leesungsup <tmdtjq0116@naver.com>
2021-06-26 01:48:49 +0900

    main

--- crawler/trans/models.py
+++ crawler/trans/models.py
@@ -10,9 +10,7 @@ class Player_info(models.Model):
     team=models.CharField(max_length=30)
     value=models.CharField(max_length=30)
 
-class Image(models.Model):
-    author=models.TextField(max_length=100)
-    photo=models.ImageField(upload_to='photos/%Y/%m/%d',default='')
-    text=models.TextField(max_length=100)
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now=True)
+class Player(models.Model):
+    name = models.CharField(max_length=20)
+    position = models.CharField(max_length=20)
+    age=models.IntegerField()
